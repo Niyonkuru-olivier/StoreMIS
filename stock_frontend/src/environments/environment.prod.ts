04b51bcc01d78dc604a56e6940d533334e4c2297
Niyonkuru-olivier <oniyonkuru44@gmail.com>
2025-06-17 10:11:08 -0700

    fix: switch to nginx for serving Angular app

--- stock_frontend/src/environments/environment.prod.ts
+++ stock_frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: (window as any).env?.API_URL || 'https://storemis-backend1.onrender.com'
+  apiUrl: (window as any).env?.API_URL_BACKEND1 || 'https://storemis-backend1.onrender.com',
+  apiUrl2: (window as any).env?.API_URL_BACKEND2 || 'https://storemis-backend2.onrender.com'
 }; 
